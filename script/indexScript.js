acd5d58e4de48d8596bd2c384db456bf7bbaabef
mtqbatutaba <101959608+mtqbatutaba@users.noreply.github.com>
2022-03-20 18:25:14 +0700

    Update indexScript.js

--- script/indexScript.js
+++ script/indexScript.js
@@ -11,21 +11,18 @@ function closeNav() {
 // -sub: icon
 document.querySelector('a.navbar-brand i').classList.add('fa', 'fa-user-circle');
 
-const icon2 = document.querySelectorAll('#mySidenav i');
-icon2[0].classList.add('fa', 'fa-times-circle-o');
-icon2[1].classList.add('fa', 'fa-whatsapp');
-icon2[2].classList.add('fa', 'fa-telegram');
+const icon1 = document.querySelectorAll('#mySidenav i');
+icon1[0].classList.add('fa', 'fa-times-circle-o');
+icon1[1].classList.add('fa', 'fa-whatsapp');
+icon1[2].classList.add('fa', 'fa-telegram');
 
-const icon3 = document.querySelector('.active i');
-icon3.classList.add('fa', 'fa-home');
+const icon2 = document.querySelectorAll('a.nav-link');
+icon2[0].classList.add('fa', 'fa-home');
+icon2[1].classList.add('fa', 'fa-file-pdf-o');
+icon2[2].classList.add('fa', 'fa-file-pdf-o');
+icon2[3].classList.add('fa', 'fa-map-marker');
+icon2[4].classList.add('fa', 'fa-chevron-circle-down');
 
-const icon4 = document.querySelectorAll('a.nav-link');
-for (let i = 1; i < icon4.length; i++) {
-  const iBaru = document.createElement('i');
-  icon4[i].appendChild(iBaru);
-  iBaru.classList.add('fa', 'fa-chevron-circle-down');
-}
-
-document.querySelector('i.waktu').classList.add('fa','fa-calendar')
+document.querySelector('i.waktu').classList.add('fa','fa-calendar');
 document.querySelector('i.lokasi').classList.add('fa','fa-map-marker');
-document.querySelector('i.daftar').classList.add('fa','fa-link');
+document.querySelector('i.daftar').classList.add('fa','fa-file-text-o');
