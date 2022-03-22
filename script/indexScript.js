6e48d7e83a8447dcf26e423ac4e13b271d9304da
mtqbatutaba <101959608+mtqbatutaba@users.noreply.github.com>
2022-03-22 21:27:58 +0700

    Update indexScript.js

--- script/indexScript.js
+++ script/indexScript.js
@@ -19,15 +19,16 @@ icon1[2].classList.add('fa', 'fa-telegram');
 const icon2 = document.querySelectorAll('a.nav-link');
 icon2[0].classList.add('fa', 'fa-home');
 icon2[1].classList.add('fa', 'fa-file-pdf-o');
-icon2[2].classList.add('fa', 'fa-link');
-icon2[3].classList.add('fa', 'fa-map-marker');
-icon2[4].classList.add('fa', 'fa-chevron-circle-down');
+icon2[2].classList.add('fa', 'fa-calendar');
+icon2[3].classList.add('fa', 'fa-link');
+icon2[4].classList.add('fa', 'fa-map-marker');
+icon2[5].classList.add('fa', 'fa-chevron-circle-down');
 
 document.querySelector('i.waktu').classList.add('fa','fa-calendar');
 document.querySelector('i.lokasi').classList.add('fa','fa-map-marker');
 document.querySelector('i.daftar').classList.add('fa','fa-link');
 
 document.querySelector('.timeline i').classList.add('fa', 'fa-calendar');
-document.querySelector('.file i').classList.add('fa', 'fa-file-pdf-o');
+document.querySelector('.fileUndangan i').classList.add('fa', 'fa-file-pdf-o');
 document.querySelector('.link i').classList.add('fa', 'fa-link');
 document.querySelector('.map i').classList.add('fa', 'fa-map-marker');
